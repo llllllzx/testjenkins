bf15508cbfeedd5b9fb1e50ecf7cee0514b47bae
llllllzx <llllllllll_zx@163.com>
2019-10-26 12:52:50 +0800

    Initial commit

---
+++ src/main/java/com/bupt/testjenkins/controller/HelloController.java
@@ -0,0 +1,15 @@
+package com.bupt.testjenkins.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by zhaoxinliu on 2019/10/26
+ */
+@RestController
+public class HelloController {
+    @RequestMapping("/hello")
+    public String hello(){
+        return "hello world lzx";
+    }
+}
