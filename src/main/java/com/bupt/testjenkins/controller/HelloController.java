51338429d0b12c42a3ee0709074ab1b00307c9bb
llllllzx <llllllllll_zx@163.com>
2019-10-29 00:11:18 +0800

    'fff'

--- src/main/java/com/bupt/testjenkins/controller/HelloController.java
+++ src/main/java/com/bupt/testjenkins/controller/HelloController.java
@@ -10,6 +10,6 @@ package com.bupt.testjenkins.controller;
 public class HelloController {
     @RequestMapping("/hello")
     public String hello(){
-        return "hello world lzx loves liangya";
+        return "hello world lzx loves yaya";
     }
 }
